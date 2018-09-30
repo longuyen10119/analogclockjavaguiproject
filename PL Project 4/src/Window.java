02ce04dd1cb4170a4cc4dc20aea6cb0a17735a1a
longnguyen <longnguyen@192.168.0.25>
2018-10-01 08:28:55 +1000

    set up canvas

--- PL Project 4/src/Window.java
+++ PL Project 4/src/Window.java
@@ -1,9 +1,14 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Window {
 	private String title;
 	private int size;
 	private JFrame frame;
+	private Canvas canvas;
 	
 	public Window(String title, int size) {
 		this.title = title;
@@ -12,6 +17,7 @@ public class Window {
 	}
 	
 	public void display() {
+		// Set up frame
 		frame = new JFrame(title);
 		frame.setSize(size, size);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,7 +25,12 @@ public class Window {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		
-	
+		// Set up canvas
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(size, size));
+		canvas.setBackground(new Color(58, 135, 170));
+		frame.add(canvas);
+		frame.pack();
 		
 				
 	}
